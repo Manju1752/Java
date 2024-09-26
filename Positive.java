fda3284e9b715d8c90dc34f03576aa3c67fc7057
Manju1752 <163723947+Manju1752@users.noreply.github.com>
2024-09-26 19:24:35 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,15 @@
+//write java program to find out given number is positive or negative.
+
+public class Positive{
+public static void main(String args[])
+{
+int n =20;
+if(n>0)
+{
+System.out.println("Given number is Positive");
+}
+else{
+System.out.println("Given number is Negative");
+}
+}
+}
