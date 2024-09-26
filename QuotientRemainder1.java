68f48394e548e3c245f9c7515fdf7afd9fca4258
Manju1752 <163723947+Manju1752@users.noreply.github.com>
2024-09-26 19:22:28 +0530

    Add files via upload

---
+++ QuotientRemainder1.java
@@ -0,0 +1,21 @@
+// write java program to find out Quotient and reminder of two numbers.
+
+
+public class QuotientRemainder1 {
+public static void main(String[] args) {
+int dividend = 10; 
+int divisor = 3;          
+if (divisor == 0) {
+System.out.println("Error: Division by zero is not allowed.");
+} else {
+            
+int quotient = dividend / divisor;
+int remainder = dividend % divisor;
+
+System.out.println("Dividend: " + dividend);
+System.out.println("Divisor: " + divisor);
+System.out.println("Quotient: " + quotient);
+System.out.println("Remainder: " + remainder);
+}
+}
+}
